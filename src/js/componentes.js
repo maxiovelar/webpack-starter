e8a06c022b77cc975b88ec313ea3bb9bbe106531
Maxi <maxiovelar@gmail.com>
2021-03-26 16:53:31 -0300

    DOCS updated

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -3,10 +3,10 @@ import '../css/componentes.css';
 
 export const saludar = (nombre) => {
 
-    console.log('Creando etiqueta h1');
+    // console.log('Creando etiqueta h1');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre}`;
+    h1.innerText = `Bienvenido ${nombre}`;
 
     document.body.append(h1);
 }
